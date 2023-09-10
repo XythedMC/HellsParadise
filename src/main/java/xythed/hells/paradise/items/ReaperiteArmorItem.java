fc75858ce0cdc14b634586c2fb102e01a6c17778
XythedMC <liran.izak6@gmail.com>
2023-09-10 20:52:12 +0300

    Update ReaperiteArmorItem.java

--- src/main/java/xythed/hells/paradise/items/ReaperiteArmorItem.java
+++ src/main/java/xythed/hells/paradise/items/ReaperiteArmorItem.java
@@ -3,6 +3,7 @@ package xythed.hells.paradise.items;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 
+
 public class ReaperiteArmorItem extends ArmorItem{
 
 	public ReaperiteArmorItem(ArmorMaterial material, Type type, Settings settings)   {
